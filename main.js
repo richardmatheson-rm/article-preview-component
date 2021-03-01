7007bef0575dd62888352ae5a49f608ed21c14af
Richard Matheson <richardmatheson.rm@gmail.com>
2021-03-01 17:26:55 +1100

    Mobile device share button transitions

--- main.js
+++ main.js
@@ -6,6 +6,17 @@ modal.style.display = "none";
 // Get the button that opens the modal
 var button = document.getElementById("share_button");
 
+// Get the second button
+var hide_button = document.getElementById("hide_button");
+
+hide_button.addEventListener("click", function () {
+  if (modal.style.display == "block") {
+    modal.style.display = "none";
+  } else if (modal.style.display == "none") {
+    modal.style.display = "block";
+  }
+});
+
 button.addEventListener("click", function () {
   if (modal.style.display == "none") {
     modal.style.display = "block";
